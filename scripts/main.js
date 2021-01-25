9ff8d39e2911951a3ee20e12f0bef3dd33177f50
daprieu <daprieu@gmail.com>
2021-01-25 14:56:26 -0600

    Have conviction option dropdown vis

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
+import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { CriminalList } from "./criminals/CriminalList.js";
 import { OfficerList } from "./officers/OfficerList.js";
 
 
 OfficerList()
 CriminalList()
+ConvictionSelect()
