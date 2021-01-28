8cfab73843d37db23ccb7bc2649d493e47aeba80
daprieu <daprieu@gmail.com>
2021-01-28 16:20:01 -0600

    save button for notes section working

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { CriminalList } from "./criminals/CriminalList.js";
+import { saveNote } from "./notes/NoteDataProvider.js";
 import { NoteForm } from "./notes/NoteForm.js";
 import { OfficerList } from "./officers/OfficerList.js";
 import { OfficerSelect } from "./officers/OfficerSelect.js";
@@ -10,3 +11,4 @@ CriminalList()
 ConvictionSelect()
 OfficerSelect()
 NoteForm()
+saveNote()
--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,23 +1,58 @@
+import { saveNote } from "./NoteDataProvider.js"
+
 const contentTarget = document.querySelector(".noteFormContainer")
+const eventHub = document.querySelector(".container")
 
 const render = () => {
     contentTarget.innerHTML = `
     <fieldset>
+    <fieldset>
         <label for="journalDate">Entry Date:</label>
-        <input type="text" id="note-date">
+        <input type="date" id="note-date">
     </fieldset>
     <fieldset>
-        <label for="entryCard__subject subject">Suspect Notes:</label>
+        <label for="entryCard__suspect subject">Suspect:</label>
         <input type="text" id="note-suspect">
     </fieldset>
     <fieldset>
-        <label for="entryCard__text text">Notes:</label>
+        <label for="entryCard__notes text">Notes:</label>
         <input type="text" id="note-text">
     </fieldset>  
+    <fieldset>
+        <label for="entryCard__author text">Author:</label>
+        <input type="text" id="note-author">
+    </fieldset> 
+
         <button id="saveNote">Save Note</button>
+    </fieldset>
     `
 }
 
+
+// save note event listener
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+        const date = document.getElementById("note-date").value
+        const suspect = document.getElementById("note-suspect").value
+        const text = document.getElementById("note-text").value
+        const author = document.getElementById("note-author").value
+
+
+        // Make a new object representation of a note
+        const newNote = {
+            // Key/value pairs here
+            date: date,
+            suspect: suspect,
+            text: text,
+            author: author,
+        }
+
+        // Change API state and application state
+        
+        saveNote(newNote)
+    }
+})
 
 export const NoteForm = () => {
     render()
